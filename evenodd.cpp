c2a4f600df8375719d68d369ae294a12792f9b47
Shashwat Sharma <shashwatsharma850@gmail.com>
2024-10-18 19:54:18 +0530

    Create evenodd.cpp

---
+++ evenodd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+    if(n%2==0){
+        cout<<"Your no is even\n";
+    }
+    else{
+        cout<<"Your no is not even it is odd";
+    }
+    
+  
+    return 0;
+}
